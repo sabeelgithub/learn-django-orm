a3628004e6e3fe4a17a12bab6f8d173eb18903f1
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2023-03-17 04:26:41 +1400

    first commit

---
+++ orm/practice/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):  
+    teacher_name = models.CharField(max_length=200)  
+  
+    def __str__(self):  
+        return f'{self.teacher_name}' 
+    
+
+class Student(models.Model):  
+    username = models.CharField(max_length=20)  
+    first_name = models.CharField(max_length=30)  
+    last_name = models.CharField(max_length=30)  
+    mobile = models.CharField(max_length=10)  
+    email = models.EmailField()  
+    teacher_name = models.ForeignKey(Teacher, blank = True, null = True, on_delete= models.CASCADE)
+  
+    def __str__(self):  
+        return "%s %s" % (self.first_name, self.last_name)
+    
+
+class jobs(models.Model):
+    job_name = models.CharField(max_length=100)
+
+    
